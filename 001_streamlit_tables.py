54aef6047e15bc96695423107538a17c23ebb6f1
j-33-t <prasenjeet.rathore@gmail.com>
2022-05-18 15:14:49 +0200

    inital commit - 18/05/2022

---
+++ 001_streamlit_tables.py
@@ -0,0 +1,31 @@
+import streamlit as st 
+import numpy as np
+import pandas as pd
+
+st.write("# Display DataFrame")
+
+
+
+# // st.write : Write arguments to the app
+st.title("St.write")
+
+st.write(pd.DataFrame({
+    'first column': [1,2,3,4],
+    'second column': [10,20,30,40]
+}))
+
+# // method for interactive table generation
+st.title("St.dataframe")
+st.write("interactive table generation")
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
+
+
+# // Method for static table generation
+st.title("St.Table")
+st.write("static table generation")
+dataframe = pd.DataFrame(
+    np.random.randn(10, 12),
+    columns=('col %d' % i for i in range(12)))
+st.table(dataframe)
